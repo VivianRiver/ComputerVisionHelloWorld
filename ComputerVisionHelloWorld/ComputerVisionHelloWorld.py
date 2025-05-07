257b590c24fac0d5549b90794b7132171ab253ea
Vivian River <vivianriver2017@gmailcom>
2025-05-06 21:54:08 -0600

    add Network class and increase number of layers

--- ComputerVisionHelloWorld/ComputerVisionHelloWorld.py
+++ ComputerVisionHelloWorld/ComputerVisionHelloWorld.py
@@ -19,6 +19,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 from DenseLayer import DenseLayer
+from Network import Network
 import emnist
 import layer
 
@@ -77,12 +78,6 @@ def cross_entropy_loss(y_pred, y_true):
 def cross_entropy_derivative(y_pred, y_true):
     return y_pred - y_true
 
-def batch_samples(X, Y, batch_size):
-    n_batches = np.ceil(len(Y) / batch_size)    
-    X_batches = np.array_split(X, n_batches)
-    Y_batches = np.array_split(Y, n_batches)
-    return X_batches, Y_batches
-
 def load_image_as_input_vector(path):
     img = Image.open(path).convert("L")
     img = ImageOps.invert(img)
@@ -97,13 +92,27 @@ def init_network(n_f, n_h1n, n_h2n, n_on):
     h1Limit = np.sqrt(6 / (n_f + n_h1n))
     h1W = np.random.randn(n_h1n, n_f) * h1Limit
     h1B = np.zeros((1, n_h1n))
-    hLayer1 = DenseLayer(h1W, h1B, tanh, tanh_der)    
+    hLayer1 = DenseLayer(h1W, h1B, relu, relu_der)    
 
     # h2W holds the weights of the n_hn neurons in the second hidden layer.
     h2Limit = np.sqrt(6 / (n_f + n_h2n))
     h2W = np.random.randn(n_h2n, n_h1n) * h2Limit
     h2B = np.zeros((1, n_h2n))
-    hLayer2 = DenseLayer(h2W, h2B, tanh, tanh_der)
+    hLayer2 = DenseLayer(h2W, h2B, relu, relu_der)
+
+    # h3W holds the weights of the n_hn neurons in the second hidden layer.
+    # use same parameters as layer 2
+    h2W = np.random.randn(n_h2n, n_h1n) * h2Limit
+    h2B = np.zeros((1, n_h2n))
+    hLayer3 = DenseLayer(h2W, h2B, tanh, tanh_der)
+
+    h2W = np.random.randn(n_h2n, n_h1n) * h2Limit
+    h2B = np.zeros((1, n_h2n))
+    hLayer4 = DenseLayer(h2W, h2B, tanh, tanh_der)
+    
+    h2W = np.random.randn(n_h2n, n_h1n) * h2Limit
+    h2B = np.zeros((1, n_h2n))
+    hLayer5 = DenseLayer(h2W, h2B, tanh, tanh_der)
 
     # oW holds the weights of the n_on neuron in the output layer.    
     oLimit = np.sqrt(6 / (n_on + n_h2n))
@@ -111,68 +120,23 @@ def init_network(n_f, n_h1n, n_h2n, n_on):
     oB = np.zeros((1, n_on))
     oLayer = DenseLayer(oW, oB, softmax, mse_der)
 
-    layers = [hLayer1, hLayer2, oLayer]
+    layers = [hLayer1, hLayer2, hLayer3, hLayer4, hLayer5, oLayer]
+    network = Network(layers)
 
     # return hW, hB, oW, oB, tanh, tanh_der, softmax, cross_entropy_loss, cross_entropy_derivative
-    return layers, cross_entropy_loss, cross_entropy_derivative
-
-def forward_pass(layers, X):
-     zValues = []
-     aValues = []
-     a = X
-     for layer in layers:
-         z, a = layer.forward_pass(a)
-         zValues.append(z)
-         aValues.append(a)
-     return zValues, aValues
-
-def backward_pass(layers, X, loss_der_value, zValues, aValues):
-    # Output layer gradients    
-    grad_oW = (loss_der_value.T @ aValues[1]) / X.shape[0]
-    grad_oB = np.mean(loss_der_value, axis=0, keepdims=True)
-
-    grad_W = [grad_oW]
-    grad_B = [grad_oB]
-
-    prev_der_value = loss_der_value
-
-    # Iterate over the layers in reverse order, but skip the output layer, the last one, as we accounted for it above.
-    # for layer in layers[:-1][::-1]
-    layer_count = len(layers)
-    for i in range(layer_count)[:-1][::-1]:
-        next_layer_weights = layers[i + 1].W        
-        this_layer_z_value = zValues[i]
-        prev_layer_input = aValues[i - 1] if i > 0 else X
-        
-        dL_dA = prev_der_value @ next_layer_weights
-        dL_dZ, grad_hW, grad_hB = layers[i].backward_pass(dL_dA, this_layer_z_value, prev_layer_input)
-
-        prev_der_value = dL_dZ
-
-        grad_W.insert(0, grad_hW)
-        grad_B.insert(0, grad_hB)
-
-    return grad_W, grad_B
-        
-def update_weights_and_biases(layers, learn_rate, grad_W, grad_B):
-    layers[2].update_weights_and_biases(learn_rate, grad_W[2], grad_B[2])
-    layers[1].update_weights_and_biases(learn_rate, grad_W[1], grad_B[1])
-    layers[0].update_weights_and_biases(learn_rate, grad_W[0], grad_B[0])
+    return network, cross_entropy_loss, cross_entropy_derivative
 
 n_f = 784
 n_h1n = 32
 n_h2n = 32
 n_on = 8
 
-batch_size = 64
-epoch_count = 200
-learn_rate = 0.05
 X_emnist, Y_emnist = emnist.load_emnist_letters("c:\\temp\\emnist\\emnist-letters-train-images-idx3-ubyte.gz", "c:\\temp\\emnist\\emnist-letters-train-labels-idx1-ubyte.gz")
 indices = np.arange(X_emnist.shape[0])
 np.random.shuffle(indices)
 X = X_emnist[indices]
 Y = Y_emnist[indices]
-X_batches, Y_batches = batch_samples(X, Y, batch_size)
+
 
 a_count = Y[Y[:,0] == 1].shape[0]
 b_count = Y[Y[:,1] == 1].shape[0]
@@ -192,22 +156,12 @@ print(f_count)
 print(o_count)
 print(x_count)
 
-layers, loss_func, loss_der = init_network(n_f, n_h1n, n_h2n, n_on)
-for epoch in range(epoch_count):
-    for X_batch, Y_batch in zip(X_batches, Y_batches):
-        zValues, aValues = forward_pass(layers, X_batch)
-        oA = aValues[-1]
-        loss_der_value = oA - Y_batch  # predicted probabilities minus true one-hot labels        
-        grad_W, grad_B = backward_pass(layers, X_batch, loss_der_value, zValues, aValues)        
-        update_weights_and_biases(layers, learn_rate, grad_W, grad_B)
-    
-    if epoch % 100 == 0 or epoch == epoch_count -1:
-        _, aValues = forward_pass(layers, X)
-        oA = aValues[-1]
-        mse_loss = mse(oA, Y)
-        print(f"epoch {epoch} | MSE: {np.mean(mse_loss):.10f}")  
-        loss = loss_func(oA, Y)
-        print(f"epoch {epoch} | CE: {np.mean(loss):.10f}")  
+network, loss_func, loss_der = init_network(n_f, n_h1n, n_h2n, n_on)
+epoch_count = 1000
+batch_size = 64
+learn_rate = 0.05
+
+network.train(X, Y, cross_entropy_loss, cross_entropy_derivative, epoch_count, batch_size, learn_rate)
 
 match_count = 0
 mismatch_count = 0
@@ -215,14 +169,14 @@ total_count = 0
 mismatches = []
 def check_result(input_vector, expected):        
     global match_count, mismatch_count, total_count  # <-- add this line
-    _, aValues = forward_pass(layers, input_vector)
+    _, aValues = network.forward_pass(input_vector)
     oA = aValues[-1]    
     prediction = rounded = (oA >= 0.5).astype(int)        
     if (np.array_equal(expected.flatten(), prediction.flatten())):
         match_count += 1        
     else:
         mismatch_count += 1
-        class_names = ["A", "B", "C", "D", "O", "X"]  # Adjust for your classes
+        class_names = ["A", "B", "C", "D", "E", "F", "O", "X"]  # Adjust for your classes
         expected_label = np.argmax(expected)
         predicted_label = np.argmax(oA)
         mismatches.append((input_vector, expected_label, predicted_label))
---
+++ ComputerVisionHelloWorld/Network.py
@@ -0,0 +1,74 @@
+import numpy as np
+
+class Network:
+    def __init__(self, layers):
+        self.layers = layers
+        
+    def train(self, X, Y, loss_function, loss_der, epoch_count, batch_size, learn_rate):
+        X_batches, Y_batches = self.batch_samples(X, Y, batch_size)
+        for epoch in range(epoch_count):
+            for X_batch, Y_batch in zip(X_batches, Y_batches):
+                zValues, aValues = self.forward_pass(X_batch)
+                oA = aValues[-1]
+                loss_der_value = oA - Y_batch  # predicted probabilities minus true one-hot labels        
+                grad_W, grad_B = self.backward_pass(X_batch, loss_der_value, zValues, aValues)        
+                self.update_weights_and_biases(learn_rate, grad_W, grad_B)
+    
+            if epoch % 100 == 0 or epoch == epoch_count - 1:
+                _, aValues = self.forward_pass(X)
+                oA = aValues[-1]
+                # mse_loss = mse(oA, Y)
+                # print(f"epoch {epoch} | MSE: {np.mean(mse_loss):.10f}")  
+                loss = loss_function(oA, Y)
+                print(f"epoch {epoch} | CE: {np.mean(loss):.10f}")  
+        return;
+
+    @staticmethod
+    def batch_samples(X, Y, batch_size):
+        n_batches = np.ceil(len(Y) / batch_size)    
+        X_batches = np.array_split(X, n_batches)
+        Y_batches = np.array_split(Y, n_batches)
+        return X_batches, Y_batches
+
+    def forward_pass(self, X):
+        zValues = []
+        aValues = []
+        a = X
+        for layer in self.layers:
+            z, a = layer.forward_pass(a)
+            zValues.append(z)
+            aValues.append(a)
+        return zValues, aValues
+
+    def backward_pass(self, X, loss_der_value, zValues, aValues):
+        # Output layer gradients    
+        grad_oW = (loss_der_value.T @ aValues[1]) / X.shape[0]
+        grad_oB = np.mean(loss_der_value, axis=0, keepdims=True)
+
+        grad_W = [grad_oW]
+        grad_B = [grad_oB]
+
+        prev_der_value = loss_der_value
+
+        # Iterate over the layers in reverse order, but skip the output layer, the last one, as we accounted for it above.
+        # for layer in layers[:-1][::-1]
+        layer_count = len(self.layers)
+        for i in range(layer_count)[:-1][::-1]:
+            next_layer_weights = self.layers[i + 1].W        
+            this_layer_z_value = zValues[i]
+            prev_layer_input = aValues[i - 1] if i > 0 else X
+        
+            dL_dA = prev_der_value @ next_layer_weights
+            dL_dZ, grad_hW, grad_hB = self.layers[i].backward_pass(dL_dA, this_layer_z_value, prev_layer_input)
+
+            prev_der_value = dL_dZ
+
+            grad_W.insert(0, grad_hW)
+            grad_B.insert(0, grad_hB)
+
+        return grad_W, grad_B
+        
+    def update_weights_and_biases(self, learn_rate, grad_W, grad_B):
+        layer_count = len(self.layers)
+        for i in range(layer_count):
+            self.layers[i].update_weights_and_biases(learn_rate, grad_W[i], grad_B[i])
